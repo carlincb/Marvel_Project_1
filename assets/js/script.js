732bbcc9b5170f5abdf2dc656f1bd6605690309d
John Gallagher <gallagherj0913@gmail.com>
2021-10-06 18:58:47 -0600

    js for random fact and fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,27 @@
+var publicKey = "9a86508c139659fd39ae10d9e08ad609";
+var privateKey = "6c71587e6cb4343819fe479a9d6cb26e66e14af3";
+console.log(publicKey);
+console.log(privateKey);
+var ts = Date.now();
+console.log(ts);
+var hash = (ts + privateKey + publicKey);
+
+var md5Hash = md5(hash);
+console.log(md5Hash);
+var tsURL = JSON.stringify(ts);
+
+// create a current timestamp, both as a parameter to calculate your hash
+// so we need to just look up how to calculate a timestamp
+// then, combine them with the format
+
+let currentURL = "http://gateway.marvel.com/v1/public/comics?ts=" + tsURL + "&apikey=" + publicKey + "&hash=" + md5Hash;
+console.log(currentURL);
+
+fetch(currentURL)
+    .then((response) => response.json())
+    .then(function (data) {
+        console.log(data);
+    });
 
   // Function and array for random fact
 
